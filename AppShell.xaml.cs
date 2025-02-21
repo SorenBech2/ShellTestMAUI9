00012433e7b657f76ad2a6a89f8e60790ae688ca
Søren Bech <soren-bech@outlook.com>
2025-02-21 15:48:39 +0100

    Add navigation routes and new Page5 with navigation
    Updated AppShell.xaml to include routes for ShellContent elements, enabling navigation. Modified AppShell.xaml.cs to log navigation events using System.Diagnostics. Corrected namespace and bound BackButtonBehavior command in SecondPage5.xaml. Updated ShellTestMAUI9.csproj to remove Windows target framework, add NuGet audit mode, AOT compilation, Android linking, and LLVM properties, and include Page5.xaml. Added new Page5.xaml with a ScrollView, image, and button for navigation to SecondPage5. Added Page5.xaml.cs with binding context and navigation event handler.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace ShellTestMAUI9
+﻿using System.Diagnostics;
+
+namespace ShellTestMAUI9
 {
     public partial class AppShell : Shell
     {
@@ -8,6 +10,16 @@
             Routing.RegisterRoute(nameof(SecondPage3), typeof(SecondPage3));
             Routing.RegisterRoute(nameof(SecondPage5), typeof(SecondPage5));
             Routing.RegisterRoute(nameof(SecondPage6), typeof(SecondPage6));
+        }
+
+        protected override void OnNavigating(ShellNavigatingEventArgs args)
+        {
+            base.OnNavigating(args);
+            Debug.WriteLine($"OnNavigating works fine.");
+            if (args == null || args.Current == null || args.Target == null)
+                return;
+            Debug.WriteLine($"Coming from : {args.Current.Location.ToString()}");
+            Debug.WriteLine($"Going to : {args.Target.Location.ToString()}");
         }
     }
 }
---
+++ Page5.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace ShellTestMAUI9
+{
+    public partial class Page5 : ContentPage
+    {
+        private readonly MainViewModel _viewModel;
+        
+        public Page5(MainViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+            _viewModel = viewModel;
+        }
+
+        private async void GoToSecondPage_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"{nameof(SecondPage5)}", true);
+        }
+    }
+
+}
